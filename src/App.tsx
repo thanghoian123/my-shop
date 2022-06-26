1cb19ebe76008e10c492d7f8c4909dfca83f84ae
tp <kuthang32@gmail.com>
2022-06-26 11:05:18 +0700

    tao them

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h2>ádasd</h2>
+      {/* tao them cai nay */}
+      <h2>tao lam</h2>
     </div>
   );
 }

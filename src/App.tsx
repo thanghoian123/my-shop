c00acc5e2b7e91f8c1c804b6d8d128c7d6056387
tp <kuthang32@gmail.com>
2022-06-26 10:45:22 +0700

    test1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h2>ádasd</h2>
+      {/* them o day */}
+      <p>them vao day1</p>
     </div>
   );
 }
